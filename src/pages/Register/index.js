822383eca6f497aedaf3c09fe3405a4f67270a28
Saidinar Ali <saidinar.a@photon.in>
2023-10-05 16:58:39 +0700

    reverse code

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -9,64 +9,13 @@ import showError from '../../utils/showMessage';
 
 
 export default function Register({ navigation }) {
-  // const [form, setForm] = useForm({
-  //   fullName: '',
-  //   profession: '',
-  //   email: '',
-  //   password: ''
-  // })
-  const [isLoading, setLoading] = useState(false)
-
   const [form, setForm] = useForm({
     fullName: '',
-    category: 'dokter umum',
-    university: '',
-    str_number: '',
-    hospital_address: '',
-    gender: 'pria',
+    profession: '',
     email: '',
-    password: '',
-  });
-  const [itemCategory] = useState([
-    {
-      id: 1,
-      label: 'Dokter Umum',
-      value: 'dokter umum',
-    },
-    {
-      id: 2,
-      label: 'Psikiater',
-      value: 'psikiater',
-    },
-    {
-      id: 3,
-      label: 'Dokter Obat',
-      value: 'dokter obat',
-    },
-    {
-      id: 4,
-      label: 'Dokter Anak',
-      value: 'dokter anak',
-    },
-    {
-      id: 5,
-      label: 'Dokter Bedah',
-      value: 'dokter bedah',
-    },
-  ]);
-
-  const [itemGender] = useState([
-    {
-      id: 1,
-      label: 'Pria',
-      value: 'pria',
-    },
-    {
-      id: 2,
-      label: 'Wanita',
-      value: 'wanita',
-    },
-  ]);
+    password: ''
+  })
+  const [isLoading, setLoading] = useState(false)
 
   const onContinue = () => {
     setLoading(true)
@@ -75,27 +24,14 @@ export default function Register({ navigation }) {
         setLoading(false)
         // Signed in 
         const user = userCredential.user;
-        // const data = {
-        //   fullName: form.fullName,
-        //   profession: form.profession,
-        //   email: form.email,
-        //   uid: user.uid
-        // }
         const data = {
           fullName: form.fullName,
-          profession: form.category,
-          category: form.category,
-          rate: 0,
-          university: form.university,
-          str_number: form.str_number,
-          hospital_address: form.hospital_address,
-          gender: form.gender,
+          profession: form.profession,
           email: form.email,
-          uid: user.uid,
-        };
+          uid: user.uid
+        }
         setForm('reset')
         set(ref(database, 'users/' + user.uid), data);
-        set(ref(database, 'doctors/' + user.uid), data);
         storeData('user', data);
         navigation.navigate("UploadPhoto", data);
         // ...
@@ -123,46 +59,6 @@ export default function Register({ navigation }) {
             <Gap height={24} />
             <Input label={"Password"} value={form.password} onChangeText={(value) => setForm("password", value)} secureTextEntry />
             <Gap height={40} />
-            <Input
-            label="Kategori"
-            value={form.category}
-            onValueChange={value => setForm('category', value)}
-            select
-            selectItem={itemCategory}
-          />
-          <Gap height={24} />
-          <Input
-            label="Universitas"
-            value={form.university}
-            onChangeText={value => setForm('university', value)}
-          />
-          <Gap height={24} />
-          <Input
-            label="Nomor STR"
-            value={form.str_number}
-            onChangeText={value => setForm('str_number', value)}
-          />
-          <Gap height={24} />
-          <Input
-            label="Alamat Rumah Sakit"
-            value={form.hospital_address}
-            onChangeText={value => setForm('hospital_address', value)}
-          />
-          <Gap height={24} />
-          <Input
-            label="Jenis Kelamin"
-            value={form.gender}
-            onValueChange={value => setForm('gender', value)}
-            select
-            selectItem={itemGender}
-          />
-          <Gap height={24} />
-          <Input
-            label="Email"
-            value={form.email}
-            onChangeText={value => setForm('email', value)}
-          />
-          <Gap height={24} />
             <Button title={"Continue"} onPress={onContinue} />
           </ScrollView>
         </View>
@@ -180,6 +76,5 @@ const styles = StyleSheet.create({
   content: {
     padding: 40,
     paddingTop: 0,
-    paddingBottom: 100
   },
 })
